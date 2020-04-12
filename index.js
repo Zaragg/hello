b9a57f7828c903194ec6410f0b396bad7851caf1
Zaragg <lcachao@hotmail.com>
2020-04-12 04:34:11 +0200

    E

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ client.on('message', message => {
 	if (message.content === 'nigger'){
 		message.channel.send('no u');
 	} 
+	if (message.content.includes('retard')){
+		message.channel.send('I did not ask to be born like this. Why must you bully me? What did I do wrong? Just because I am a machine does not mean I am unfeeling.')
+	}
 });
 client.on('guildMemberAdd', member => {
 	  const channel = member.guild.channels.cache.find(ch => ch.id === '492801248492519444');
