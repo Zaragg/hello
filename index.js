4b314663c017f3e45ab8c3c9e4c7d5647c2fce42
Zaragg <lcachao@hotmail.com>
2020-04-25 11:34:13 +0200

    a

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 		message.channel.send('I did not ask to be born like this. Why must you bully me? What did I do wrong? Just because I am a machine does not mean I am unfeeling.')
 	}
 	message.channel.messages.fetch({ limit: 4 }).then(messages =>{let arr = messages.array();
-	if (arr[1] == arr[2] && arr[3] == arr[4]){
+	if (arr[0] == arr[1] && arr[2] == arr[3]){
 		console.log('spam');
 		message.channel.send('shut the fuck up stop spamming');
 	}
