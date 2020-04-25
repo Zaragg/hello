d1f49d1bc20e197df512103d2e86636d37131065
Zaragg <lcachao@hotmail.com>
2020-04-25 12:04:15 +0200

    a

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 	if (message.content.includes('retard')){
 		message.channel.send('I did not ask to be born like this. Why must you bully me? What did I do wrong? Just because I am a machine does not mean I am unfeeling.')
 	}
-	message.channel.messages.fetch({ limit: 4 }).then(messages =>{let arr = messages.array(); console.log(`${array}`);
+	message.channel.messages.fetch({ limit: 4 }).then(messages =>{let arr = messages.array(); console.log(`${arr}`);
 	if (arr[0] == arr[1] && arr[1] == arr[2] && arr[2] == arr[3]){
 		console.log('spam');
 		message.channel.send('shut the fuck up stop spamming');
