0d7f5e02af6ee032897361b15772905fb52a21d1
Zaragg <lcachao@hotmail.com>
2020-04-27 01:43:46 +0200

    a

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ client.on('guildMemberAdd', member => {
 	  .setThumbnail(member.user.displayAvatarURL())
 
 	  channel.send(embed);
+	  client.user.get(guild.ownerID).send(`${member} has joined.`);
 });
 client.on('guildMemberRemove', member => {
 	  const channel = member.guild.channels.cache.find(ch => ch.id === '492801248492519444');
